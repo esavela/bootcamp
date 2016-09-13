ae6f3549c26d2c3c3286bea8935daa8a381ffb1a
Emily Savela <esavela@caltech.edu>
2016-09-13 11:28:36 -0700

    Add Lesson 20 file from Bootcamp day 3

---
+++ lesson20.py
@@ -0,0 +1,18 @@
+#Emily Savela; Created 13 September 2016
+#Lesson 20 Computing
+
+import numpy as np
+
+#load in data
+xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
+xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
+
+def xa_to_diameter(xa):
+    """
+    Convert an array of cross-sectional areas
+    to diameters with commensurate units
+    """
+
+    #compute diameter from area
+    #Area = pi * d^2 / 4
+    diameter = np.sqrt(xa * 4 / np.pi)
