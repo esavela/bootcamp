2736749902c32d826533f6690c1bf5bfa38ce283
Emily Savela <esavela@caltech.edu>
2016-09-13 13:03:19 -0700

    Add updated code from end of Bootcamp Day 2 Lesson 20.

--- lesson20.py
+++ lesson20.py
@@ -7,6 +7,7 @@ import numpy as np
 xa_high = np.loadtxt('data/xa_high_food.csv', comments='#')
 xa_low = np.loadtxt('data/xa_low_food.csv', comments='#')
 
+
 def xa_to_diameter(xa):
     """
     Convert an array of cross-sectional areas
@@ -18,4 +19,23 @@ def xa_to_diameter(xa):
     diameter = np.sqrt(xa * 4 / np.pi)
 
     return diameter
-    
+
+
+def easy_reshape(obj, ncols, order='C'):
+    """
+    Guantee that reshaping works by declining only one
+    shape parameter
+    """
+
+    #Reshape
+    arr = np.reshape(obj, (ncols, len(obj)/ncols), order)
+
+    return arr
+
+#Arrays for practice in the command line
+A = np.array([[6.7, 1.3, 0.6, 0.7],
+              [0.1, 5.5, 0.4, 2.4],
+              [1.1, 0.8, 4.5, 1.7],
+              [0.0, 1.5, 3.4, 7.5]])
+
+b = np.array([1.1, 2.3, 3.3, 3.9])
