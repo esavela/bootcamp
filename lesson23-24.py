25e4e0c79b75d383af8427d844c849a5fc827872
Emily Savela <esavela@caltech.edu>
2016-09-13 15:51:42 -0700

    Add files from Bootcamp Day 3, Lessons 23-24; Plotting

---
+++ lesson23-24.py
@@ -0,0 +1,43 @@
+# Emily Savela; Created 13 September 2016
+# Lesson 23 and 24
+
+import numpy as np
+import scipy.stats
+
+# This is how we import the module of Matplotlib
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+rc = {'lines.linewidth' : 2, 'axes.labelsize' : 18,
+        'axes.titlesize' : 18}
+sns.set(rc=rc)
+
+data_txt = np.loadtxt('data/collins_switch.csv', delimiter=',', skiprows=2)
+
+# Slice out iptg and gfp
+iptg = data_txt[:,0]
+gfp = data_txt[:,1]
+sem = data_txt[:,2]
+
+# # Practice Exercise 1
+# # Plot ipgt vs gfp
+# plt.semilogx(iptg, gfp, linestyle='none', marker='.',
+#              markersize=20)
+# plt.xlabel('IPTG (mM)')
+# plt.ylabel('Normalized GFP')
+# plt.title('IPTG Titration - semilog X')
+# plt.ylim(-0.02,1.02)
+# plt.xlim(8e-4, 15)
+# plt.show()
+
+# # Practice Exercise 2
+# Plot iptg vs gfp
+# plt.errorbar(iptg, gfp, yerr=sem, linestyle='none',
+#              marker='.', markersize=20)
+# plt.xlabel('IPTG (mM)')
+# plt.ylabel('Normalized GFP')
+# plt.title('IPTG Titration - semilog X')
+# plt.ylim(-0.02,1.02)
+# plt.xlim(8e-4, 15)
+# plt.xscale('log')
+# plt.show()
