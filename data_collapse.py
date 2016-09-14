48e665187c7e4d0a8e5229015ad2eeedf8d2763c
Emily Savela <esavela@caltech.edu>
2016-09-13 21:24:05 -0700

    Add Data Collapse Exercise parts (a-e); Bootcamp day 3

--- data_collapse.py
+++ data_collapse.py
@@ -1,5 +1,5 @@
 # Emily Savela; Created 13 September 2016
-# Exercise 3
+# Exercise 3.3 Data Collapse (a-f)
 
 import numpy as np
 import scipy.stats
@@ -22,12 +22,31 @@ def fold_change(c, RK, KdA=0.017, KdI=0.002, Kswitch=5.8):
     RK is an array or scalar (R/K)
     KdA, KdI, and Kswitch are constants
     """
-    numerator = RK * (1+(c/KdA))**2
-    denominator = (1+(c/KdA))**2 + Kswitch * (1+(c/KdI))**2
-    fold_change = 1/(1+(numerator/denominator))
+    numerator = RK * (1 + (c/KdA))**2
+    denominator = (1 + (c/KdA))**2 + Kswitch * (1 + (c/KdI))**2
+    fold_change = 1/(1 + (numerator/denominator))
 
     return fold_change
 
+
+def bohr_parameter(c, RK, KdA=0.017, KdI=0.002, Kswitch=5.8):
+    """
+    Computes the Bohr parameter to simplify fold change calculation
+    """
+    num = (1 + c/KdA)**2
+    den = (1 + c/KdA)**2 + Kswitch * (1 + c/KdI)**2
+    bohr_par = np.log(RK) - np.log(num/den)
+
+    return bohr_par
+
+
+def fold_change_bohr(bohr_parameter):
+    """
+    Computes the fold change with given bohr parameter
+    """
+    fc = 1 / (1 + np.exp(-bohr_parameter))
+
+    return fc
 
 # Load data
 wt_lac = np.loadtxt('data/wt_lac.csv', comments='#',
@@ -45,36 +64,29 @@ q18m_fold = q18m_lac[:,1]
 q18a_IPTG = q18a_lac[:,0]
 q18a_fold = q18a_lac[:,1]
 
-# Compute theoretical concentrations
-wt_IPTG_theo = np.logspace(9e-6, 25, 500)
-q18m_IPTG_theo = np.logspace(9e-6, 25, 500)
-q18a_IPTG_theo = np.logspace(9e-6, 25, 500)
+# Compute theoretical values
+IPTG_theo = np.logspace(9e-6, 25, 500)
+bohr_theo = np.linspace(-6, 6, 500)
 
-# Compute theoretical fold Change
-wt_fold_theo = fold_change(wt_IPTG_theo, 141.5)
-q18m_fold_theo = fold_change(q18m_IPTG_theo, 16.56)
-q18a_fold_theo = fold_change(q18a_IPTG_theo, 1332)
+# Compute theoretical fold Change, with bohr parameter
+fold_theo = fold_change_bohr(bohr_theo)
 
 # Ensure no open plots
 plt.close()
 
 # Plot all experimental data
-plt.semilogx(wt_IPTG, wt_fold, marker='.', linestyle='none',
-         markersize=20, alpha=0.8)
-plt.semilogx(q18m_IPTG, q18m_fold, marker='.', linestyle='none',
-         markersize=20, alpha=0.8)
-plt.semilogx(q18a_IPTG, q18a_fold, marker='.', linestyle='none',
-         markersize=20, alpha=0.8)
+# plt.semilogx(wt_IPTG, wt_fold, marker='.', linestyle='none',
+#          markersize=20, alpha=0.8)
+# plt.semilogx(q18m_IPTG, q18m_fold, marker='.', linestyle='none',
+#          markersize=20, alpha=0.8)
+# plt.semilogx(q18a_IPTG, q18a_fold, marker='.', linestyle='none',
+#          markersize=20, alpha=0.8)
 
 # Plot theoretical data
-plt.semilogx(wt_IPTG_theo, wt_fold_theo, color='magenta')
-plt.semilogx(q18m_IPTG_theo, q18m_fold_theo, color='black')
-plt.semilogx(q18a_IPTG_theo, q18a_fold_theo, color='yellow')
-
-# Plot parameters
-plt.xlabel('Concentration IPTG (mM)')
+plt.plot(bohr_theo, fold_theo, color='gray')
+plt.xlim(-6,6)
+plt.xlabel('Bohr Parameter')
 plt.ylabel('Fold Change')
 plt.legend(('wt_lac', 'q18m_lac', 'q18a_lac',
-            'theoretical wt_lac', 'theoretical q18m_lac',
-            'theoretical q18a_lac'), loc='center right')
+            'theoretical'), loc='lower right')
 plt.show()
