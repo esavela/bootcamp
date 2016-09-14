4eefe92e777d1cf8adf0cc75d79a2a1fedf6ed72
Emily Savela <esavela@caltech.edu>
2016-09-13 21:51:17 -0700

    Add Euler Model Sample Code, Exercise 3.4 from Bootcamp Day 3

---
+++ euler_model.py
@@ -0,0 +1,38 @@
+# Emily Savela; Created 13 September 2016
+# Exercise 3.4 Euler's method of predator-prety dynamics
+# Example code from Bootcamp Day 3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import Seaborn and settings
+import seaborn as sns
+rc = {'lines.linewidth' : 2, 'axes.labelsize' : 18,
+      'axes.titlesize' : 28}
+sns.set(rc=rc)
+
+# Specify parameter
+k = 1
+
+# Specify my little time step
+delta_t = 0.01
+
+# Make an array of time points, evenly spaced up to 10
+t = np.arange(0, 10, delta_t)
+
+# Make an array to store the number of bacteria
+n = np.empty_like(t)
+
+# Set the initial number of bacteria
+n[0] = 1
+
+# Write a for loop to keep updating n as time goes on
+for i in range(1, len(t)):
+    n[i] = n[i-1] + delta_t * k * n[i-1]
+
+plt.close()
+plt.plot(t, n)
+plt.margins(0.02)
+plt.xlabel('time')
+plt.ylabel('number of bacteria')
+plt.show()
