51e480977233d35756730ae5d91cc48eb43d083d
Emily Savela <esavela@caltech.edu>
2016-09-15 21:19:09 -0700

    From Exercise 5; Bootcamp day 5

---
+++ bac_growth_curves.py
@@ -0,0 +1,12 @@
+# Emily Savela; Created 15 September 2016
+# Exercise 5
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+rc={'lines.linewidth': 2, 'axes.labelsize': 18, 'axes.titlesize': 18}
+sns.set(rc=rc)
+
+# Load data
+for 
